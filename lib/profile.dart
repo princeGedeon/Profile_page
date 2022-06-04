0609b42b390725ae5a6f10efdd2cfadf8f1fcdec
princeGedeon <guedjegedeon03@gmail.com>
2022-06-04 21:45:54 +0100

    Mise en place de la page profil

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,1 +1,67 @@
-class
+
+class Profile{
+  String nom;
+  String prenom;
+  int age;
+  bool gender;
+  double taille;
+  String secret;
+  List<String> hobbies=[];
+  String favoriteLang;
+
+  Profile({
+    this.nom="",
+    this.prenom="",
+    this.age=0,
+    this.gender=true,
+    this.taille=0.0,
+    this.secret="",
+    this.hobbies=const [],
+    this.favoriteLang="Python"
+});
+
+  String get_fullname()
+  {
+    return "$nom  $prenom";
+  }
+
+  String get_age()
+  {
+    String ageString="$age an";
+    if (age>1)
+      {
+        ageString+="s";
+      }
+    return ageString;
+  }
+
+  String get_gender()
+  {
+    return (gender)? "Féminin":"Masculin";
+  }
+
+  String get_taille()
+  {
+    return '$taille cm';
+  }
+
+  String get_hoobies(){
+    String tohobbiesstring="";
+    if (hobbies.length==0){
+      return tohobbiesstring;
+    }
+    else{
+      tohobbiesstring=" Mes hobbies sont :";
+    hobbies.forEach((element) {
+      tohobbiesstring+=" $element ,";
+    });
+    return tohobbiesstring;
+    }
+
+
+  }
+  String get_favoriteLang(){
+    return favoriteLang;
+  }
+
+}
--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,43 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:mon_profile/profile.dart';
 
-class ProfilePage extends StatefulWidget{
-
-  ProfilePageState createState()=>ProfilePageState();
+class ProfilePage extends StatefulWidget {
+  ProfilePageState createState() => ProfilePageState();
 }
 
-class ProfilePageState extends State<ProfilePage>
-{
+class ProfilePageState extends State<ProfilePage> {
+  Profile myprofile = Profile(nom: "GUEDJE",prenom: "Gédéon Prince");
+  late TextEditingController nom;
+  late TextEditingController prenom;
+  late TextEditingController secret;
 
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
+    nom=TextEditingController();
+    prenom=TextEditingController();
+    secret=TextEditingController();
   }
 
   @override
-  void dispose()
-  {
+  void dispose() {
+    nom.dispose();
+    prenom.dispose();
+    secret.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text('Mon profil'),),
+      appBar: AppBar(
+        title: Text('Mon profil'),
+      ),
       body: SingleChildScrollView(
         child: Column(
           mainAxisSize: MainAxisSize.max,
           children: [
             Card(
-              child: Container(
+                color: Colors.deepPurple.shade100,
+                elevation: 10,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
 
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  Text(myprofile.get_fullname()),
+                  Text('Age : ${myprofile.get_age()}'),
+                  Text("Taille : ${myprofile.get_taille()}"),
+                  Text('Genre : ${myprofile.get_gender()}'),
+                  Text("Hobbies : ${myprofile.get_hoobies()}"),
+                  Text("Language de programmation favori : ${myprofile.get_favoriteLang()}")
+                ],
               ),
-            )
-
+            )),
+            Divider(color: Colors.deepPurpleAccent,thickness: 2),
+            Text("Modifier les infos ",style: TextStyle(),),
           ],
         ),
       ),
     );
   }
 
+  TextField myTextField({required TextEditingController controller,required String hint,bool isSecret=false})
+  {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hint,
+
+      ),
+      obscureText: isSecret,
+    );
+  }
 }
